7be36f032a49c10e79b7239ec9ea887cfd36bac8
Lawrence Chao <Lawrencechao777@gmail.com>
2025-11-12 23:19:23 +0800

    rebase main

---
+++ Assets/MainGame/Scripts/FoodCard.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FoodCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public string foodName;
+    private CanvasGroup canvasGroup;
+    private Transform originalParent;
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent;
+        transform.SetParent(transform.root); // 提升層級避免被 UI 蓋住
+        canvasGroup.blocksRaycasts = false; // 避免卡牌阻擋 Drop 區
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / transform.root.localScale.x;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (transform.parent == transform.root)
+        
+        canvasGroup.blocksRaycasts = true;
+    }
+}
